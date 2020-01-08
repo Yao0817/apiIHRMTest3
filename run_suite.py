21f4ec3f51ab7b76cba0647938a030b195523de1
Yao0817 <59526944+Yao0817@users.noreply.github.com>
2020-01-08 14:55:36 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -15,7 +15,8 @@ suite.addTest(unittest.makeSuite(TestLogin))
 suite.addTest(unittest.makeSuite(TestIHRMEmp))
 # suite.addTest(unittest.makeSuite(TestIHRMLogin))
 # 3.使用HTMLTestRunner执行测试套件，生成测试报告
-report_path = app.BASE_DIR +"/report/ihrm{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+#report_path = app.BASE_DIR +"/report/ihrm{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+report_path = app.BASE_DIR +"/report/ihrm.html"
 
 with open(report_path,mode='wb') as f:
     #初始化HestRunner
